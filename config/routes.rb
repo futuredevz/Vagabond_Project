32887cbcb6072cc4a2a6fc7f18b370a1018fbe1f
Chris Pak <chris.pak125@gmail.com>
2018-10-26 16:26:46 -0400

    test seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ namespace :api do
+  resources :users do
+    resources :posts
+  end
+end
+namespace :api do
+  resources :cities do
+    resources :posts
+  end
+end
+
 end
