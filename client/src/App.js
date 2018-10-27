8dbf878da42756862cb84e725bc52fa446f1bbfa
Chris Ducote <chrisd@slappy.local>
2018-10-26 21:23:36 -0400

    Navbar added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path='/' component={LandingPage}/>
-        </Switch>
+        <div>
+          <NavBar/>
+          <Switch>
+            <Route exact path='/' component={LandingPage}/>
+          </Switch>
+        </div>
       </Router>
     );
   }
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,8 +4,7 @@ export default class LandingPage extends Component {
   render() {
     return (
       <div>
-        <h1>Vagabond</h1>
-        <hr/>
+        
       </div>
     )
   }
