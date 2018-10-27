a17bc047f577201c778d041bf5ca9239fab4af84
Chris Ducote <chrisd@slappy.local>
2018-10-26 20:57:07 -0400

    added react-router to app.js LandingPage created

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Vagabond</h1>
+        <hr/>
+      </div>
+    )
+  }
+}
