37d0fbf37bc2e26de63b92ea01ffa4bb501808d2
Chris Ducote <chrisd@slappy.local>
2018-10-27 10:37:44 -0400

    designed navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,18 +3,32 @@ import styled from 'styled-components'
 
 const Navbar = styled.div`
   display: flex;
-  justify-content: center;
-  font: 56px 'Damion', cursive;;
+  justify-content: space-between;
+  /* font: 56px 'Damion', cursive; */
   padding-top: 10px;
   padding-bottom: 10px;
   border-bottom: 1px solid black;
 `
+const LeftBox = styled.div`
+  font: 56px 'Noto Sans', sans-serif;
+  font-weight: bold;
+  font-style: italic;
+  align-self: flex-start;
+  padding-left: 20px;
+`
+const RightBox = styled.div`
+  font: 32px 'Noto Sans', sans-serif;
+  align-self: flex-end;
+  padding-right: 20px;
+`
+
 
 export default class NavBar extends Component {
   render() {
     return (
       <Navbar>
-        Vagabond
+        <LeftBox>Vagabond</LeftBox>
+        <RightBox>Sign In</RightBox>
       </Navbar>
     )
   }
