137d109dc4016b1fc21e31f0cb0c39b0e9238cbc
Chris Pak <chris.pak125@gmail.com>
2018-10-29 09:23:08 -0400

    Posts create route

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
+import SingleCity from './components/SingleCity';
+import SinglePost from './components/SinglePost';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+      <Switch>
+        <Route exact path ='/cities/:id' component={SingleCity} /> 
+        <Route exact path ='/cities/:city_id/posts/:id' component={SinglePost} /> 
+      </Switch>
+    </Router>
+
     );
   }
 }
