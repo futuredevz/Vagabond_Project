5c5e91366ea2cc761aac393c408058fcb52c0084
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-10-29 14:40:56 -0400

    added 2 cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,7 @@
 
 user = User.create(name: "Chris", current_city: "Atl", img: "something")
 city = City.create(name: "Atlanta", img: "https://i.pinimg.com/originals/ef/e5/77/efe57732e72644aeb14367eac4394c9a.jpg")
+city = City.create(name: "Los Angeles", img: "https://www.visitmacysusa.com/sites/default/files/styles/hero/public/macys-los-angeles-skyline-at-night-header.jpg?itok=FTikBOPJ")
+city = City.create(name: "Dallas", img: "https://xixerone.com/en/wp-content/uploads/sites/2/2017/09/Best-areas-to-stay-in-Dallas-Texas.jpg")
+
 post = Post.create(title: "test", body: "this is a test", user_id: 1, city_id: 1 )
